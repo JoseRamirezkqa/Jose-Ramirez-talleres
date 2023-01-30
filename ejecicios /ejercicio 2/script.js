10c5473f123b51f54c4b8f669a94507d4eb6cfff
JoseRamirezkqa <123585701+JoseRamirezkqa@users.noreply.github.com>
2023-01-30 18:49:00 -0500

    Delete ejecicios  directory

--- ejecicios /ejercicio 2/script.js
+++
@@ -1,7 +0,0 @@
-let arreglo = ['hola', 2,5,7,2,8,`JuanJo`,14,`tablero`,`cable`];
-let arreglo2 = arreglo.filter((el) => {
-    if(isNaN(el)) {
-        return el;
-    }
-}) 
-console.log(arreglo2);
