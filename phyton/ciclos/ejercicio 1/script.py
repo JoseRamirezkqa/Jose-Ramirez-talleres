3a044cf682bc65cfdadb7e117337bdfcaab6a3fa
JoseRamirezkqa <jose.ramirezcacua@gmail.com>
2023-02-05 21:14:44 -0500

    empezar talleres python

---
+++ phyton/ciclos/ejercicio 1/script.py
@@ -0,0 +1,12 @@
+n  = 0 
+m = 1
+c = 0
+secuencias = input("¿Cuántas veces quiere que se realice?: ")
+secuencias = int(secuencias)
+print(n)
+print(m)
+for secuencias in range(2,secuencias):
+    c = n+m; 
+    print(c)
+    n=m
+    m=c
